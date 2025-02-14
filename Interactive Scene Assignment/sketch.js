887e127e6ca3ec342b0178a958abf8ff0a58768b
Maheen-S07 <maheenshahid987@gmail.com>
2025-02-14 12:17:07 -0600

    Work Period 3- Interactie Assignment

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -2,27 +2,30 @@
 // Maheen Shahid
 // Feb 12, 2025
 
-//Global Variable Declarations
 
-let boxX;
-let boxY;
 
 function setup() {
   createCanvas(1000, 1000);
-  boxX = width/2;
-  boxY = height/2;
 }
 
 function draw() {
   background(220);
   drawBackground();
+  drawCharacter();
 }
 
 function drawBackground() {
   fill(230, 224, 158);
   rect(0, 600, windowWidth, 200);
-  triangle(300, 200, 0, 600, 600, 600);
   fill(105, 105, 100);
   triangle(300, 200, 0, 600, 600, 600);
-  triangle(750, 200, 400, 600, 1000, 600);
+  fill(128, 128, 120);
+  triangle(700, 200, 400, 600, 1000, 600);
+}
+
+function drawCharacter(){
+  fill(207, 85, 74)
+  rect(98, 550, 5, 110);
+  fill(252, 252, 252);
+  circle(100, 510, 90);
 }
