722d0543492b91154d1a56defe3bc19dfdc537c7
Maheen-S07 <maheenshahid987@gmail.com>
2025-02-23 22:26:21 -0600

    Interactive Scene Changes

--- Interactive Scene Assignment/sketch.js
+++ Interactive Scene Assignment/sketch.js
@@ -4,23 +4,23 @@
 
 
 //Global Variable Declaration
-
 let jimX = 100; // starts jim at x = 100
+let currentBack = 0 //Default Background
 
 function setup() {
   createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(220);
+  background(209, 238, 240);
   drawBackground();
   jimTheCharacter(jimX);
 
       // Character Movement
       if(keyIsDown(LEFT_ARROW)){
         jimX -= 5;
-        if(jimX < 0){  // off the edge
-          jimX = 1000;  // create a wrap around effect
+        if(jimX < 0){  // off the screen
+          jimX = 1000;  
           }
       }
       if(keyIsDown(RIGHT_ARROW)){
@@ -29,16 +29,47 @@ function draw() {
           jimX = 0;
         }
       }
+
 }
 
 function drawBackground() { // Draws Scenery
   noStroke();
-  fill(230, 224, 158);
+
+  if(currentBack === 0){   // Default Blue Sky
+    background(209, 238, 240);
+  }
+
+  if(currentBack === 1){   // Sunset
+    background(255, 140, 0);
+  }
+
+  if(currentBack === 2){   // Night
+    background(30, 30, 30);
+  }
+
+  if(currentBack === 3){   // Snowy
+    background(250, 249, 242);
+  }
+
+  fill(232, 200, 42);
+  circle(800, 100, 110);
+
+  fill(197, 199, 199);
   rect(0, 600, windowWidth, 200);
-  fill(105, 105, 100);
+
+  fill(162, 196, 157);
+  rect(0, 800, windowWidth, 200);
+
+  fill(105, 105, 100);                     // Mountains
   triangle(300, 200, 0, 600, 600, 600);
-  fill(128, 128, 120);
+  fill(128, 128, 120);                       
   triangle(700, 200, 400, 600, 1000, 600);
+
+
+  // Name 
+  fill('tomato');
+  text("Maheen Shahid", 5, 990);
+  textSize(25);
 }
 
 function jimTheCharacter(x){ // Draws interactive character
@@ -52,11 +83,24 @@ function jimTheCharacter(x){ // Draws interactive character
   circle(x, 510, 90); // Head moves with x
 
   // ARMS
+
   line(x + 2, 590, x - 13, 650);
-  line(x + 4, 590, x + 15, 650);
+  line(x + 4, 590 , x + 15, 650);
   // LEGS
   line(x + 2, 660, x - 13, 710);
   line(x + 4, 660, x + 15, 710);
 
+}
+
+function mousePressed(){
+  if(mouseButton === CENTER){
+
+      currentBack = (currentBack + 1);  // Changes Background
+
+      if(currentBack > 3){
+        currentBack === 0
+    }
+
+  }
 }
 
