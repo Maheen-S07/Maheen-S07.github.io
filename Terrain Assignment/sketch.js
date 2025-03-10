95a79ed10353596a808f25f26a4fd077b998ffd2
Maheen-S07 <maheenshahid987@gmail.com>
2025-03-09 22:55:14 -0600

    Terrain Assignment updates

---
+++ Terrain Assignment/sketch.js
@@ -0,0 +1,42 @@
+// Terrain Generation Project
+// Maheen Shahid
+// March 5, 2025
+
+
+let rectWidth = 1;
+let noiseOffset = 0;
+let noiseSpeed = 0.1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  generateTerrain();
+}
+
+function generateTerrain(){
+  // use a loop to generate and draw
+  // several rectangles side to side
+  // to look like some 2D terrain
+  rectMode(CORNERS);
+
+  for(let x = 0; x < width; x += rectWidth){
+    //generate a random height. 
+    //change this from using random() to noise()
+    let rectHeight = noise(noiseOffset);
+    rectHeight = map(rectHeight, 0, 1, 50, 500);
+    
+    //calculate the other corner of our rectangle
+    let x2 = x + rectWidth;
+    let y2 = height - rectHeight;
+
+    rect(x, height, x2, y2);
+
+    noiseOffset += noiseSpeed; //move through noise
+  }
+  
+  rectMode(CORNER);
+}
+
+function draw() {
+  // background(220);
+  //generateTerrain();
+}
