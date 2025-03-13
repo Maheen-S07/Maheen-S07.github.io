820a2bde7e3ab3f200520e50d1c94aa856651b90
Maheen-S07 <maheenshahid987@gmail.com>
2025-03-13 10:40:55 -0600

    March 13

--- 13 Objects Demo (Books)/sketch.js
+++ 13 Objects Demo (Books)/sketch.js
@@ -12,6 +12,14 @@ let myBook;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //Make 20 books in a row
+  let x = 0;
+  let covers = ["softcover", "hardcover", "leatherbound"];
+  for(let i = 0, i<20; i++){
+    let choice = int(random(3)); // 0,1,2
+
+    bookshelf.push(new Book("A", "Mr Booth", 1111111111))
+  }
   myBook = new Book("CS30 Text", "Mr. Scott", 1234567891011, "leatherbound", 515, width * 0.3);
   myBook.printOut();
 }
