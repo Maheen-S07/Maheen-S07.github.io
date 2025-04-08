4f4b0888b2964e510dd8c064d13bfe47f6ab00a8
Maheen-S07 <maheenshahid987@gmail.com>
2025-04-08 13:19:04 -0600

    April 8

--- Assignments and Challenges/Cars Cars Cars/sketch.js
+++ Assignments and Challenges/Cars Cars Cars/sketch.js
@@ -5,13 +5,18 @@
 // Global Variables
 let eastbound = [];
 let westbound = [];
+let trafficLight;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  //Add traffic light
+  trafficLight = new TrafficLight(width - 60, 20);
+
   // 20 Eastbound Cars
   for( let i = 0; i < 20; i ++){
     let x = random(width);
-    let y = random(height/4 + 20, height/4 + 120); //Top half
+    let y = random(height/4 + 20, height/4 + 110); //Top half
     let newCar = new Vehicle(x, y, color(random(255), random(255), random(255)));
     newCar.xSpeed = abs(newCar.xSpeed); // always positive -> moving right
     eastbound.push(newCar);
@@ -20,16 +25,18 @@ function setup() {
   // 20 Westbound Cars
   for( let i = 0; i < 20; i ++){
     let x = random(width);
-    let y = random(height/4 + 180, height/4 + 280); //Bottom half
+    let y = random(height/4 + 160, height/4 + 270); //Bottom half
     let newCar = new Vehicle(x, y, color(random(255), random(255), random(255)));
     newCar.xSpeed = -abs(newCar.xSpeed); // always negative -> moving left
     westbound.push(newCar);
   }
+
 }
 
 function draw() {
   background(220);
   drawRoad();
+  trafficLight.action();
   for(let car of eastbound){
     car.action();
   }
@@ -94,8 +101,11 @@ class Vehicle{
   }
 
   move(){
-    // Movement of the vehicles on screen
-    this.x += this.xSpeed;
+    //Movement of the vehicles on screen
+    //Only move if light is green
+    if(trafficLight.color === "green"){
+      this.x += this.xSpeed;
+    }
 
     // Wrap around the screen
     if(this.x > width){
@@ -159,6 +169,88 @@ class Vehicle{
     }
    }
   }
+
+  function mousePressed(){
+    //Add vehicles on road
+    //Right Click -> Eastbound car
+    //Left Click ->  Westbound car
+
+    if(mouseButton === RIGHT){
+      let x = random(width);
+      let y = random(height/4 + 20, height/4 + 110); //Top half
+      let newCar = new Vehicle(x, y, color(random(255), random(255), random(255)));
+      newCar.xSpeed = abs(newCar.xSpeed); 
+      eastbound.push(newCar);
+    }
+    if(mouseButton === LEFT){
+      let x = random(width);
+      let y = random(height/4 + 160, height/4 + 270); //Bottom half
+      let newCar = new Vehicle(x, y, color(random(255), random(255), random(255)));
+      newCar.xSpeed = -abs(newCar.xSpeed); // always negative -> moving left
+      westbound.push(newCar);
+    }
+  }
+
+class TrafficLight {
+  constructor(x, y){
+    this. x = x;
+    this.y = y;
+    this.color = "green"; // Starts at green
+    this.timer = 0;
+  }
+
+  display(){
+    // Show traffic light
+    fill(50);
+    rect(this.x, this.y,40, 100);
+
+    //Red Light
+    if(this.color === "red"){
+      fill(235, 51, 38);
+    }
+    else{
+      fill(99, 120, 92);
+    }
+    ellipse(this.x + 20, this.y + 25, 20);
+
+    //Green Light
+    if(this.color === "green"){
+      fill(126, 232, 90);
+    }
+    else{
+      fill(99, 120, 92);
+    }
+    ellipse(this.x + 20, this. y + 75, 20);
+  }
+
+
+  action(){
+    //Update the light
+    this.display()
+
+    if(this.color === "red"){
+      this.timer--; //count down
+      if(this.timer <= 0){
+        this.color = "green";
+      }
+    }
+  }
+
+  turnRed(){
+    //Turns the light to red
+    this.color = "red";
+    this.timer = 120; //Stays red for 120 frames
+  }
+
+}
+
+function keyPressed(){
+  //Allows user to stop traffic
+  //Turns light red for 120 fps
+  if(keyCode === 32){
+    trafficLight.turnRed();
+  } 
+}
 
 
 
---
+++ Assignments and Challenges/Puzzle Game Assignment/sketch.js
@@ -0,0 +1,112 @@
+// Puzzle Game
+// Maheen Shahid
+// April 8, 2025        
+
+let NUM_ROWS = 4;
+let NUM_COLS = 5;
+let rectWidth, rectHeight;
+let currentRow, currentCol;
+let gridData = [
+  [0,   0,   0,   0, 0],
+  [0,   0,   0,   0, 0],
+  [0,   255, 0,   0, 0],
+  [255, 255, 255, 0, 0]];
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectWidth = width/NUM_COLS;
+  rectHeight = height/NUM_ROWS;
+  randomStart();
+}
+
+function draw() {
+  background(220);
+  determineActiveSquare();   
+  drawGrid(); 
+  winner();
+}
+
+
+
+function mousePressed(){
+  // cross-shaped pattern flips on a mouseclick.
+  // Boundary conditions are checked within the flip function to ensure
+  // in-bounds access for array
+  if(keyIsDown(SHIFT)){
+    flip(currentCol, currentRow);
+  }
+  else{
+    flip(currentCol, currentRow);
+    flip(currentCol-1, currentRow);
+    flip(currentCol+1, currentRow);
+    flip(currentCol, currentRow-1);
+    flip(currentCol, currentRow+1);
+  }
+}
+
+function flip(col, row){
+  // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
+  // conditions ensure that the col and row given are valid and exist for the array.
+  // If not, no operations take place.
+  if (col >= 0 && col < NUM_COLS ){
+    if (row >= 0 && row < NUM_ROWS){
+      if (gridData[row][col] === 0) gridData[row][col] = 255;
+      else gridData[row][col] = 0;
+    }
+  }
+}
+
+function determineActiveSquare(){
+  // An expression to run each frame to determine where the mouse currently is.
+  currentRow = int(mouseY / rectHeight);
+  currentCol = int(mouseX / rectWidth);
+}
+
+function drawGrid(){
+  // Render a grid of squares - fill color set according to data stored in the 2D array
+  for (let x = 0; x < NUM_COLS ; x++){
+    for (let y = 0; y < NUM_ROWS; y++){
+      fill(gridData[y][x]); 
+      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
+    }
+  }
+}
+
+function winner(){
+  // Check if all elements are 0 or 255
+  // display "You Win"
+  let firstValue = gridData[0][0]; //compare all other numbers to this
+  let allSame = true;
+  for(let x = 0; x < NUM_COLS; x++){
+    for(let y = 0; y < NUM_ROWS; y++){
+      if(gridData[y][x] !== firstValue){
+        allSame = false;
+        break;
+      }
+    }
+  }
+  if(allSame){ // Completed puzzle
+    fill(255, 105, 180);
+    textSize(64);
+    textAlign(CENTER, CENTER);
+    textStyle(BOLD);
+    text("You Win!", width / 2, height / 2);  
+  }
+}
+
+function randomStart(){
+  //Display a random pattern each reset
+  for(let x = 0; x < NUM_COLS; x++){
+    for(let y = 0; y < NUM_ROWS; y++){
+      let randomValue = random(255);
+      if(randomValue > 127){
+        gridData[y][x] = 255;
+      }
+      else{
+        gridData[y][x] = 0;
+      }
+    }
+  }
+}
