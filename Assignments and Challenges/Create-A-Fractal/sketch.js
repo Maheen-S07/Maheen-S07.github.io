ad7efed76f4386e512cd23a07f0b7f31d19f9406
Maheen-S07 <maheenshahid987@gmail.com>
2025-05-07 13:53:18 -0600

    Final fractal

--- Assignments and Challenges/Create-A-Fractal/sketch.js
+++ Assignments and Challenges/Create-A-Fractal/sketch.js
@@ -2,28 +2,53 @@
 // Maheen Shahid
 // April 17, 2025
 
+//Global Variables
+let angle = 0;
 
 function setup() {
-  createCanvas(600,600);
-  background(0);
-  stroke(255);
-  drawFractal(150, 150, 300);
+  createCanvas(600,600, WEBGL);
+  strokeWeight(1);
+  noFill();
+}
+
+function draw(){
+  background(255);
+
+  rotateX(angle);
+  rotateY(angle);
+
+  drawCircle(0, 0, width/2, 0);
+  // update angle
+  angle += 0.1; 
 }
 
 
-function drawFractal(x,y,size){
-  if(size <10){
+
+function drawCircle(x, y, d){
+  //Draw one main circle
+  //Draw 8 circles around each circle on the canvas recursively
+  //Each circle decreasing in size
+
+  //BASE CASE
+  if(d < 10){
     return;
   }
+  //Random color for each circle
+  stroke(random(255), random(255), random(255));
+  push();
+  translate(x,y,0);
+  sphere(d/2);
+  pop();
 
-  fill(random(255), random(255), random(255));
-  rect(x, y, size, size);
-
-  let newSize = size/3;
-
-  drawFractal(x-newSize, y - newSize, newSize);
-  drawFractal(x+ size, y - newSize, newSize);
-  drawFractal(x-newSize, y + size, newSize);
-  drawFractal(x + size, y + size, newSize);
-
+  let radius = d/2;
+  //Recursive Calls
+  drawCircle(x + radius, y, radius);
+  drawCircle(x - radius, y, radius);
+  drawCircle(x, y + radius, radius);
+  drawCircle(x, y - radius, radius);
+  drawCircle(x + radius, y + radius, radius);
+  drawCircle(x - radius, y - radius, radius);
+  drawCircle(x - radius, y + radius, radius);
+  drawCircle(x + radius, y - radius, radius);
 }
+
