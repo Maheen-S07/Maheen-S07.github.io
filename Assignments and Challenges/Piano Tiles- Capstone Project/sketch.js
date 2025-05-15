f0699c53ed93ea2f79f46452c8e74b7fb2b1f171
Maheen-S07 <maheenshahid987@gmail.com>
2025-05-14 21:23:31 -0600

    Capstone work
    I continued to fix the scrolling issue with my game. I was still not able to get it 100% working. I missed class today so I did not have much time to finish

--- Assignments and Challenges/Piano Tiles- Capstone Project/sketch.js
+++ Assignments and Challenges/Piano Tiles- Capstone Project/sketch.js
@@ -6,8 +6,6 @@ let NUM_ROWS = 4
 let NUM_COLS = 4
 let rectWidth;
 let rectHeight;
-let currentCol;
-let currentRow;
 let pianoTiles = [];
 let scrollY = 0;
 let scrollSpeed = 2;
@@ -15,42 +13,57 @@ let scrollSpeed = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectWidth = windowWidth/NUM_COLS;
-  rectHeight = windowHeight/NUM_ROWS;
+  setUpSizes();
   randomStart();
 }
 
 function draw() {
   background(220);
-  scrollY += scrollSpeed;
   drawPiano();
+  updateTiles();
+  scrollY += scrollSpeed;
+}
+
+function setUpSizes(){
+  //Set size of each tile based on size of screen
+  //Uses rows/cols to divide
+  rectWidth = windowWidth/NUM_COLS;
+  rectHeight = windowHeight/NUM_ROWS;
+}
+
+function randomStart(){
+  //Create the starting rows for the game
+  //helps scroll smoothly by adding more tiles
+  let totalRows = NUM_ROWS + 2; 
+  for(let i = 0; i < totalRows; i++){
+    pianoTiles.push(randomRow());
+  }
 }
 
 function drawPiano(){
   //Render a grid of squares - 4x4
-  //Equally divided depending on size of screen
-  for (let x = 0; x < NUM_COLS ; x++){
-    for (let y = 0; y < NUM_ROWS; y++){
-      fill(pianoTiles[y][x]); 
-      rect(x*rectWidth, (y*rectHeight + scrollY) % windowHeight - rectHeight, rectWidth, rectHeight);
+  //Use scrollY to scroll smoothly
+  for (let y = 0; y < pianoTiles.length; y++){ 
+    for (let x = 0; x < NUM_COLS; x++){
+      let tileY = y * rectHeight + scrollY;
+      fill(pianoTiles[row][x]); 
+      rect(x*rectWidth, tileY -  rectHeight, rectWidth, rectHeight);
     }
   }
 }
 
-function randomStart(){
-  //A different arrangement of tiles everytime
-  //Make one random tile from each row black.
-  //The black tiles are what the user clicks.
-  pianoTiles = [];
-  for (let i = 0; i <NUM_ROWS; i ++){
-  pianoTiles.push(randomRow());
-}
+function updateTiles(){
+  //Keep a flow of new rows
+  // Check if
+  if(scrollY >= rectHeight){
+    scrollY -= rectHeight;
+    pianoTiles.pop(); //remove bottom row
+    pianoTiles.unshift(randomRow()); //add new row
+  }
 }
 
+
 function randomRow(){
-  //A different arrangement of tiles everytime
-  //Make one random tile from each row black.
-  //The black tiles are what the user clicks.
   let row = [];
   let blackTile = floor(random(NUM_COLS)); //Choose random coloumn in the row
   for(let x = 0; x < NUM_COLS; x++){
@@ -62,6 +75,11 @@ function randomRow(){
   }
  }
   return row;
+}
+
+function shiftTilesDown(){
+  pianoTiles.pop(); //remove bottom row
+  pianoTiles.unshift(randomRow()); //add new row
 }
 
 
