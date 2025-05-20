61657f7dbc7a54eddb53fcc18c0011e38721a2ac
Maheen-S07 <maheenshahid987@gmail.com>
2025-05-20 12:13:07 -0600

    Capstone work- May 20
    Today I worked with cordova and was able to download it. However, we found it may be easier to just load it on a web browser using the touch functions rather than creating an app so I tested that out.

---
+++ testingtouch/sketch.js
@@ -0,0 +1,29 @@
+let value = 0;
+
+function setup() {
+  createCanvas(100, 100);
+
+  describe(
+    'A gray square with a black square at its center. The inner square switches color between black and white each time the user stops touching the screen.'
+  );
+}
+
+function draw() {
+  background(200);
+
+  // Style the square.
+  fill(value);
+
+  // Draw the square.
+  square(25, 25, 50);
+}
+
+// Toggle colors when a touch ends.
+function touchEnded() {
+  if (value === 0) {
+    value = 255;
+  } else {
+    value = 0;
+  }
+}
+
