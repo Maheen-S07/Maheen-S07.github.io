9cb34af02c832cd874bc12f3b7e08eb30e7e6506
Maheen-S07 <maheenshahid987@gmail.com>
2025-05-27 22:35:39 -0600

    capstone work
    The switch to OOP wasn't working out so i went back to my 2D array concept.

--- Assignments and Challenges/PianoTiles/sketch.js
+++ Assignments and Challenges/PianoTiles/sketch.js
@@ -25,38 +25,48 @@ function draw() {
 }
 
 // ----------- Display and Functionality of Game --------------
-class Tile{
-  constructor(col,row,color){
-    this.col = col;
-    this.row = row;
-    this.color = color;
-    this.y = row * rectHeight - (rectHeight * 2); //starts a little off screen 
-  }
 
-  update(){
-    this.y += scrollSpeed;
-  }
+function setUpSizes(){
+  //Set size of each tile based on size of screen
+  //Uses rows/cols to divide
+  rectWidth = windowWidth/NUM_COLS;
+  rectHeight = windowHeight/NUM_ROWS;
+}
 
-  draw(){
-    let x = this.col * rectWidth
-    fill(this.color);
-    rect(x,this.y, rectWidth, rectHeight);
+function randomStart(){
+  //Create the starting rows for the game
+  //helps scroll smoothly by adding more tiles
+  let totalRows = NUM_ROWS + 2; 
+  for(let i = 0; i < totalRows; i++){
+    pianoTiles.push(randomRow());
   }
+}
 
-  touchAction(){
-    if(this.color === 'black'){ //correct tile
-      this.color = 'white';
+function drawPiano(){
+  //Render a grid of squares - 4x4
+  //Use scrollY to scroll smoothly
+  for (let y = 0; y < pianoTiles.length; y++){ 
+    for (let x = 0; x < NUM_COLS; x++){
+      let tileY = y * rectHeight + scrollY;
+      fill(pianoTiles[y][x]); 
+      rect(x*rectWidth, tileY -  rectHeight, rectWidth, rectHeight);
     }
-    else if(this.color === 'white'){ //wrong tile
-      this.color = 'red';
-    }
   }
-    
+  scrollY += scrollSpeed;
+}
 
+function updateTiles(){
+  //Keep a flow of new rows
+  // Check if a full row has passed
+  if(scrollY >= rectHeight){
+    scrollY -= rectHeight;
+    pianoTiles.pop(); //remove bottom row
+    pianoTiles.unshift(randomRow()); //add new row
+  }
 }
 
 
-function createRow(){
+function randomRow(){
   //create a row, one black tile and the rest white
   //used for everytime a new row enters
   let row = [];
@@ -72,57 +82,30 @@ function createRow(){
   return row;
 }
 
-
-function setUpSizes(){
-  //Set size of each tile based on size of screen
-  //Uses rows/cols to divide
-  rectWidth = windowWidth/NUM_COLS;
-  rectHeight = windowHeight/NUM_ROWS;
-}
-
-function randomStart(){
-  //Create the starting rows for the game
-  //helps scroll smoothly by adding more tiles
-  let totalRows = NUM_ROWS + 2; 
-  for(let i = 0; i < totalRows; i++){
-    pianoTiles.push(createRow());
-  }
-}
-
-
-
-
-function updateTiles(){
-  //Keep a flow of new rows
-  //Check if a full row has passed
-  for(let row of pianoTiles){
-    for(let tile of row){
-      Tile.update();
-    }
-  }
-}
-
-function drawPiano(){
-  //Render a grid of squares - 4x4
-  //Use scrollY to scroll smoothly
-  for (let y = 0; y < pianoTiles.length; y++){ 
-    for (let x = 0; x < NUM_COLS; x++){
-      let tileY = y * rectHeight + scrollY;
-      fill(pianoTiles[y][x]); 
-      rect(x*rectWidth, tileY - rectHeight, rectWidth, rectHeight);
-    }
-  }
-  scrollY += scrollSpeed;
-}
-
-
-
-
-
 // ------- Touch -----------
 
+function tileTouch(x,y){
+  for (let y = 0; y < pianoTiles.length; y++){}
+    for (let x = 0; x < NUM_COLS; x++){
+      let tileX = x * rectWidth;
+      let tileY = y * rectHeight + scrollY - rectHeight;
+      //where did we click?
+      if(x > tileX && x < tileX + rectWidth && y > tileY && y < tileY + rectHeight){
+        let color = pianoTiles[y][x];
+        if(color === 'black'){
+          pianoTiles[y][x] = 'white';
+        }
+        else if(color === 'white'){  //clicked the wrong tile
+          pianoTiles[y][x] = 'red'
+        }
+        }
+
+      }
+
+}
+
 function touchStarted(){
-  for (let row = 0; row < pianoTiles.length; row++){}
+  for (let row = 0; y < pianoTiles.length; row++){}
     for (let col = 0; col < NUM_COLS; col++){
       let tileX = col * rectWidth;
       let tileY = row * rectHeight + scrollY - rectHeight;
@@ -141,15 +124,3 @@ function touchStarted(){
       }
   return false;
 }
-
-
-
-
-
-
-
-
-
-
-
-
